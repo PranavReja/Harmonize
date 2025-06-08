398e57fa1530bd43d327c6e4691ec66ecba6a226
PranavReja <kukreja.pranav1@gmail.com>
2025-06-07 23:28:24 -0400

    Searchresultbar

--- Harmonize/src/components/SearchResultCard.jsx
+++ Harmonize/src/components/SearchResultCard.jsx
@@ -14,10 +14,10 @@ export default function SearchResultCard({
         <div className="song-title">{title}</div>
         <div className="artist-name">{artist}</div>
       </div>
-      <div className="service-info">{service}</div>
+
       <div className="action-buttons">
         <button className="add-to-queue-button" onClick={onAdd}>+</button>
-        <button className="play-next-button" onClick={onPlayNext}>↑</button>
+        <button className="play-next-button" onClick={onPlayNext}>→</button>
 
       </div>
     </div>
